2f8dbc6aad02b950d7fa33633e15759891bfb084
arghblargh <elsonchin27@gmail.com>
2018-10-22 23:20:03 -0700

    added growth rates

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ var port = process.env.PORT || 8080;
 app.set('json spaces', 2);
 
 app.get('/', function (req, res) {
-    res.send('FEH Stats API: /stats/{rarity}-{level}');
-})
+    res.send('FEH Stats API: /stats/{rarity}-{level}<br />\
+              Unit Growths: /stats/growths');
+});
 
 app.get('/stats/:rarity-:level', function (req, res) {
   var maxLevel = req.params.level == 40 ? true : false;
@@ -27,11 +28,27 @@ app.get('/stats/:rarity-:level', function (req, res) {
     }
     res.json(formatStats(JSON.parse(body)));
   });
-})
+});
+
+app.get('/stats/growths', function (req, res) {
+  var formData = {
+    action: 'cargoquery',
+    format: 'json',
+    limit: '500',
+    tables: 'HeroGrowths',
+    fields: '_pageName=Name,HP,Atk,Spd,Def,Res'
+  }
+  request.post({ url:'https://feheroes.gamepedia.com/api.php', formData: formData }, function (err, response, body) {
+    if (err) {
+      return console.error('request failed: ', err);
+    }
+    res.json(formatStats(JSON.parse(body)));
+  });
+});
 
 app.listen(port, function () {
     console.log('Listening on port ' + port)
-})
+});
 
 function formatStats(data) {
   var result = {};
